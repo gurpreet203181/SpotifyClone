b8c3548d085cf5020cc88eb1a47275dcaf7e38fc
gurpreet203181 <45843946+gurpreet203181@users.noreply.github.com>
2023-01-27 18:21:58 +0100

    Project Setup

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 
+import recentlyplayed from 'src/assets/mockdata/recentlyPlayed.json';
+import heavyRotation from 'src/assets/mockdata/heavyRotation.json';
+import jumpBackIn from 'src/assets/mockdata/jumpBackIn.json';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
+  data = [
+    {
+      title: 'Recently Played',
+      albums: recentlyplayed,
+    },
+    {
+      title: 'Heavy Rotation',
+      albums: heavyRotation,
+    },
+    {
+      title: 'Jump Back In',
+      albums: jumpBackIn,
+    },
+  ];
   constructor() {}
-
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
-
+  @ViewChild(IonTabs) tabs!: IonTabs;
+  selected: string = '';
+  progress = '42';
   constructor() {}
 
+  setSelectedTab() {
+    console.log('called');
+    this.selected = String(this.tabs.getSelected());
+  }
 }
